755d927fb8caa7a6814bc32c64a72437c54730aa
preko06 <istvan.orban@workl.com>
2024-07-25 15:30:32 +0200

    feat: init

--- src/app/new-page/page.tsx
+++ src/app/new-page/page.tsx
@@ -1,8 +1,13 @@
+import {cookies} from "next/headers";
+
 export default function NewPage() {
+    const authToken = cookies().get('auth_token');
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-center p-24">
             <h1 className="text-4xl font-bold mb-4">This page is served from Vercel</h1>
             <p className="text-xl">You are viewing the new-page route.</p>
+            <p className="text-xl">Your auth token is: {authToken?.value}</p>
         </main>
     );
 }
